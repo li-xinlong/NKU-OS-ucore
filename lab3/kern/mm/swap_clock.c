3b5ecb8dc1c8e8d7a1a6226e7100accee7374215
li-xinlong <lixlgood@foxmail.com>
2024-10-30 04:40:13 -0400

    练习3完成

--- lab3/kern/mm/swap_clock.c
+++ lab3/kern/mm/swap_clock.c
@@ -32,13 +32,16 @@ list_entry_t pra_list_head, *curr_ptr;
  */
 static int
 _clock_init_mm(struct mm_struct *mm)
-{     
-     /*LAB3 EXERCISE 4: YOUR CODE*/ 
-     // 初始化pra_list_head为空链表
-     // 初始化当前指针curr_ptr指向pra_list_head，表示当前页面替换位置为链表头
-     // 将mm的私有成员指针指向pra_list_head，用于后续的页面替换算法操作
-     //cprintf(" mm->sm_priv %x in fifo_init_mm\n",mm->sm_priv);
-     return 0;
+{
+    /*LAB3 EXERCISE 4: 2212599 2212294 2212045*/
+    // 初始化pra_list_head为空链表
+    // 初始化当前指针curr_ptr指向pra_list_head，表示当前页面替换位置为链表头
+    // 将mm的私有成员指针指向pra_list_head，用于后续的页面替换算法操作
+    // cprintf(" mm->sm_priv %x in fifo_init_mm\n",mm->sm_priv);
+    list_init(&pra_list_head);
+    curr_ptr = &pra_list_head;
+    mm->sm_priv = &pra_list_head;
+    return 0;
 }
 /*
  * (3)_fifo_map_swappable: According FIFO PRA, we should link the most recent arrival page at the back of pra_list_head qeueue
@@ -46,14 +49,16 @@ _clock_init_mm(struct mm_struct *mm)
 static int
 _clock_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int swap_in)
 {
-    list_entry_t *entry=&(page->pra_page_link);
- 
+    list_entry_t *entry = &(page->pra_page_link);
+
     assert(entry != NULL && curr_ptr != NULL);
-    //record the page access situlation
-    /*LAB3 EXERCISE 4: YOUR CODE*/ 
+    // record the page access situlation
+    /*LAB3 EXERCISE 4: 2212599 2212294 2212045*/
     // link the most recent arrival page at the back of the pra_list_head qeueue.
     // 将页面page插入到页面链表pra_list_head的末尾
     // 将页面的visited标志置为1，表示该页面已被访问
+    list_add_before((list_entry_t *)mm->sm_priv, entry);
+    // page-> = 1;
     return 0;
 }
 /*
@@ -61,16 +66,17 @@ _clock_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, in
  *                            then set the addr of addr of this page to ptr_page.
  */
 static int
-_clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
+_clock_swap_out_victim(struct mm_struct *mm, struct Page **ptr_page, int in_tick)
 {
-     list_entry_t *head=(list_entry_t*) mm->sm_priv;
-         assert(head != NULL);
-     assert(in_tick==0);
-     /* Select the victim */
-     //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
-     //(2)  set the addr of addr of this page to ptr_page
-    while (1) {
-        /*LAB3 EXERCISE 4: YOUR CODE*/ 
+    list_entry_t *head = (list_entry_t *)mm->sm_priv;
+    assert(head != NULL);
+    assert(in_tick == 0);
+    /* Select the victim */
+    //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
+    //(2)  set the addr of addr of this page to ptr_page
+    while (1)
+    {
+        /*LAB3 EXERCISE 4: 2212599 2212294 2212045*/
         // 编写代码
         // 遍历页面链表pra_list_head，查找最早未被访问的页面
         // 获取当前页面对应的Page结构指针
@@ -80,70 +86,75 @@ _clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tic
     return 0;
 }
 static int
-_clock_check_swap(void) {
+_clock_check_swap(void)
+{
 #ifdef ucore_test
     int score = 0, totalscore = 5;
     cprintf("%d\n", &score);
-    ++ score; cprintf("grading %d/%d points", score, totalscore);
+    ++score;
+    cprintf("grading %d/%d points", score, totalscore);
     *(unsigned char *)0x3000 = 0x0c;
-    assert(pgfault_num==4);
+    assert(pgfault_num == 4);
     *(unsigned char *)0x1000 = 0x0a;
-    assert(pgfault_num==4);
+    assert(pgfault_num == 4);
     *(unsigned char *)0x4000 = 0x0d;
-    assert(pgfault_num==4);
+    assert(pgfault_num == 4);
     *(unsigned char *)0x2000 = 0x0b;
-    ++ score; cprintf("grading %d/%d points", score, totalscore);
-    assert(pgfault_num==4);
+    ++score;
+    cprintf("grading %d/%d points", score, totalscore);
+    assert(pgfault_num == 4);
     *(unsigned char *)0x5000 = 0x0e;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     *(unsigned char *)0x2000 = 0x0b;
-    assert(pgfault_num==5);
-    ++ score; cprintf("grading %d/%d points", score, totalscore);
+    assert(pgfault_num == 5);
+    ++score;
+    cprintf("grading %d/%d points", score, totalscore);
     *(unsigned char *)0x1000 = 0x0a;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     *(unsigned char *)0x2000 = 0x0b;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     *(unsigned char *)0x3000 = 0x0c;
-    assert(pgfault_num==5);
-    ++ score; cprintf("grading %d/%d points", score, totalscore);
+    assert(pgfault_num == 5);
+    ++score;
+    cprintf("grading %d/%d points", score, totalscore);
     *(unsigned char *)0x4000 = 0x0d;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     *(unsigned char *)0x5000 = 0x0e;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     assert(*(unsigned char *)0x1000 == 0x0a);
     *(unsigned char *)0x1000 = 0x0a;
-    assert(pgfault_num==6);
-    ++ score; cprintf("grading %d/%d points", score, totalscore);
-#else 
+    assert(pgfault_num == 6);
+    ++score;
+    cprintf("grading %d/%d points", score, totalscore);
+#else
     *(unsigned char *)0x3000 = 0x0c;
-    assert(pgfault_num==4);
+    assert(pgfault_num == 4);
     *(unsigned char *)0x1000 = 0x0a;
-    assert(pgfault_num==4);
+    assert(pgfault_num == 4);
     *(unsigned char *)0x4000 = 0x0d;
-    assert(pgfault_num==4);
+    assert(pgfault_num == 4);
     *(unsigned char *)0x2000 = 0x0b;
-    assert(pgfault_num==4);
+    assert(pgfault_num == 4);
     *(unsigned char *)0x5000 = 0x0e;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     *(unsigned char *)0x2000 = 0x0b;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     *(unsigned char *)0x1000 = 0x0a;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     *(unsigned char *)0x2000 = 0x0b;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     *(unsigned char *)0x3000 = 0x0c;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     *(unsigned char *)0x4000 = 0x0d;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     *(unsigned char *)0x5000 = 0x0e;
-    assert(pgfault_num==5);
+    assert(pgfault_num == 5);
     assert(*(unsigned char *)0x1000 == 0x0a);
     *(unsigned char *)0x1000 = 0x0a;
-    assert(pgfault_num==6);
+    assert(pgfault_num == 6);
 #endif
     return 0;
 }
-
 
 static int
 _clock_init(void)
@@ -159,17 +170,18 @@ _clock_set_unswappable(struct mm_struct *mm, uintptr_t addr)
 
 static int
 _clock_tick_event(struct mm_struct *mm)
-{ return 0; }
-
+{
+    return 0;
+}
 
 struct swap_manager swap_manager_clock =
-{
-     .name            = "clock swap manager",
-     .init            = &_clock_init,
-     .init_mm         = &_clock_init_mm,
-     .tick_event      = &_clock_tick_event,
-     .map_swappable   = &_clock_map_swappable,
-     .set_unswappable = &_clock_set_unswappable,
-     .swap_out_victim = &_clock_swap_out_victim,
-     .check_swap      = &_clock_check_swap,
+    {
+        .name = "clock swap manager",
+        .init = &_clock_init,
+        .init_mm = &_clock_init_mm,
+        .tick_event = &_clock_tick_event,
+        .map_swappable = &_clock_map_swappable,
+        .set_unswappable = &_clock_set_unswappable,
+        .swap_out_victim = &_clock_swap_out_victim,
+        .check_swap = &_clock_check_swap,
 };
