c80bc3459f1676f8f2e71230cceee0f434984358
zsy <zhaosiyang2004@petalmail.com>
2024-11-13 16:00:24 +0800

    修复了检查中的小问题

--- lab3/kern/mm/swap.c
+++ lab3/kern/mm/swap.c
@@ -38,7 +38,7 @@ int swap_init(void)
      {
           panic("bad max_swap_offset %08x.\n", max_swap_offset);
      }
-     sm = &swap_manager_fifo;
+     sm = &swap_manager_clock;
      //  sm = &swap_manager_clock;//use first in first out Page Replacement Algorithm
      int r = sm->init();
 
